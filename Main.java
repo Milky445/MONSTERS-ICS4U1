792fd3fd23139db8d4d2c7cb3853a86cc7a3aa25
Santi Motoc <notvixcy@gmail.com>
2023-10-02 19:38:36 -0400

    final copy?? i stg im quitting ts

--- Main.java
+++ Main.java
@@ -1,17 +1,24 @@
-import java.util.Scanner;
-public class Main {
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Main extends Monster {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter type: ");
+        System.out.println("Please enter type of monster 2: ");
         String typeMonster = input.nextLine();
-        System.out.println("Please enter name: ");
+
+        System.out.println("Please enter name of monster 2: ");
         String nameMonster = input.nextLine();  // local variable
-        System.out.println("Please enter age: " );
+
+        System.out.println("Please enter age of monster 2: ");
         int ageMonster = input.nextInt();
+
         System.out.println("Bad breath?(true/false): ");
         boolean badBreathMonster = input.nextBoolean();
-        System.out.println("Please enter gpa: ");
-        double gpaMonster = input.nextDouble();// another local variable
+
+        System.out.println("Please enter gpa of monster 2: ");
+        double gpaMonster = input.nextDouble(); // another local variable
+
         input.nextLine();  // to handle the end of line characters
 
         System.out.println("Please enter the name of monster 3: ");
@@ -23,6 +30,15 @@ public class Main {
         Monster monster1 = new Monster();
         Monster monster3 = new Monster(nameMonster3, ageMonster3);
         Monster monster2 = new Monster(typeMonster, nameMonster, ageMonster, badBreathMonster, gpaMonster);
+        // System.out.println("NEW MONSTER 2");
+        // System.out.println("------------");
+        monster2.changeName("Santi");
+        monster2.changeAge(1000);
+        monster2.changeType("Vampire");
+        monster2.changeBadBreath(true);
+        monster2.changeGPA(1.2);
+
+        // System.out.println("------------");
         /*
         System.out.println("type: " + monster2.typeMonster);
         System.out.println("name: " + monster2.nameMonster);
@@ -31,27 +47,150 @@ public class Main {
         System.out.println("gpa: " + monster2.gpaMonster);
 
          */
-        System.out.println("Would you like to give your monster a spa day?: ");
-        boolean spaDay = input.nextBoolean();
+
         System.out.println("How much will your spa day cost?: ");
         double priceSpaDay = input.nextDouble();
-        monster2.spaDay(ageMonster, gpaMonster, priceSpaDay);
-        if (spaDay == true) {
+        input.nextLine();
+        System.out.println("MONSTER 2 SPA DAY");
+        System.out.println("------------------------------");
+        monster1.spaDay(ageMonster, gpaMonster, priceSpaDay);
+        System.out.println("New bad breath: " + badBreathMonster);
+        System.out.println("New gpa: " + gpaMonster);
+        System.out.println("Price: " + priceSpaDay);
+        System.out.println("New age: " + ageMonster);
+        System.out.println("------------------------------");
+        System.out.println("MONSTER 3 SPA DAY");
+        System.out.println("------------------------------");
+        monster3.spaDay(ageMonster3, gpaMonster, priceSpaDay);
+        System.out.println("New bad breath: " + badBreathMonster);
+        System.out.println("New gpa: " + gpaMonster);
+        System.out.println("Price: " + priceSpaDay);
+        System.out.println("New age: " + ageMonster3);
+
+        System.out.println("------------------------------");
+            /*
             System.out.println("Which monster would you like to give a spa day?: ");
             String whichMonster = input.nextLine();
-            if (whichMonster == "monster1") {
+            if (Objects.equals(whichMonster, "monster 1")) {
                 monster1.spaDay(ageMonster, gpaMonster, priceSpaDay);
-            } else if {
-                // add else if statement here for monster2 and monster3
+            } else if (Objects.equals(whichMonster, "monster 2")) {
+                monster2.spaDay(ageMonster, gpaMonster, priceSpaDay);
+            } else if (Objects.equals(whichMonster, "monster 3")) {
+                monster3.spaDay(ageMonster3, gpaMonster, priceSpaDay);
+            }
+             */
+        System.out.println("By how many years would you like to time warp?: ");
+        double numberOfYears = input.nextDouble();
+        input.nextLine();
+        System.out.println("MONSTER 1 TIME WARP");
+        System.out.println("------------------------------");
+        monster1.timeWarp(numberOfYears);
+        System.out.println("The Monster's new age is " + ageMonster);
+        System.out.println("The Monster's new gpa is " + gpaMonster);
+        System.out.println("------------------------------");
+        System.out.println("MONSTER 2 TIME WARP");
+        System.out.println("------------------------------");
+        monster2.timeWarp(numberOfYears);
+        System.out.println("The Monster's new age is " + ageMonster);
+        System.out.println("The Monster's new gpa is " + gpaMonster);
+        System.out.println("------------------------------");
+
+        // Extra feature (****)
+        // List<Monster> monsters = new ArrayList<Monster>();
+        /*
+        // boolean createNewMonster = false;
+        System.out.println("Would you like to create one new monster? (yes/no): ");
+        String newMonster = input.nextLine();
+        // Monster monster4 = null;
+        Monster monster4 = null;
+        if (Objects.equals(newMonster, "yes")) {
+            // createNewMonster = true;
+            System.out.println("Type: ");
+            typeMonster = input.nextLine();
+            System.out.println("Name: ");
+            nameMonster = input.nextLine();
+            System.out.println("Age: ");
+            ageMonster = input.nextInt();
+            System.out.println("Bad Breath?: ");
+            badBreathMonster = input.nextBoolean();
+            System.out.println("GPA: ");
+            gpaMonster = input.nextDouble();
+            monster4 = new Monster(typeMonster, nameMonster, ageMonster, badBreathMonster, gpaMonster);
+            monsters.add(monster4);
+            // System.out.println("Would you like to create a new monster? (yes/no): ");
+            // newMonster = input.nextLine();
+        }
+        if (Objects.equals(newMonster, "no")) {
+            System.out.println("Moving on .... ");
+        }
+
+         */
+
+        // System.out.println(monsters);
+        // while (createNewMonster == true) {
+            /*
+            System.out.println("Please enter type of monster 2: ");
+            String typeMonster = input.nextLine();
+            System.out.println("Please enter name of monster 2: ");
+            String nameMonster = input.nextLine();  // local variable
+            System.out.println("Please enter age of monster 2: " );
+            int ageMonster = input.nextInt();
+            System.out.println("Bad breath?(true/false): ");
+            boolean badBreathMonster = input.nextBoolean();
+            System.out.println("Please enter gpa of monster 2: ");
+            double gpaMonster = input.nextDouble();// another local variable
+            input.nextLine();  // to handle the end of line characters
+
+             */
+        // }
+
+        // System.out.println("Percent Evil: ");
+        monster3.percentEvil();
+        monster2.percentEvil();
+        System.out.println();
+        // assert monster4 != null;
+        // Extra feature (**)
+        /*
+        try {
+            monster4.percentEvil();
+        } catch (NullPointerException e) {
+            System.out.println("monster4 is null");
+        }
+
+        monsters.add(monster1);
+        monsters.add(monster2);
+        monsters.add(monster3);
+
+        System.out.println(monsters);
+
+         */
+        System.out.println(monster1);
+        System.out.println(monster2);
+        System.out.println(monster3);
+
+        // System.out.println(monster1);
+        // System.out.println(monster2);
+        // System.out.println(monster3);
+        /*
+        System.out.println("Which monster would you like to time warp?: ");
+        String whichTimeWarp = input.nextLine();
+        if (Objects.equals(whichTimeWarp, "monster 1")) {
+            monster1.timeWarp(numberOfYears);
+        } else if (Objects.equals(whichTimeWarp, "monster 2")) {
+            monster2.timeWarp(numberOfYears);
+        } else if (Objects.equals(whichTimeWarp, "monster 3")) {
+            monster3.timeWarp(numberOfYears);
             }
 
-        }
-        System.out.println("Would you like to time warp your monster?: ");
-        boolean timeWarp = input.nextBoolean();
-        if (timeWarp == true) {
-            System.out.println("By how many years would you like to time warp?: ");
-            double numberOfYears = input.nextDouble();
-            monster2.timeWarp(numberOfYears);
-        }
-        }
+         */
+        /*
+        String[] arr = {nameMonster};
+        // System.out.println(Arrays.toString(arr));
+        // for (int i = 0; i < arr.length; i++) {
+        arr = Arrays.copyOf(arr, arr.length + 1);
+        arr[arr.length - 1] = nameMonster; // Assign nameMonster to the last element
+        System.out.println(Arrays.toString(arr));
+
+         */
+    }
     }
--- Monster.java
+++ Monster.java
@@ -1,106 +1,234 @@
-import java.util.Random;
 import java.util.Scanner;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Arrays;
 public class Monster {
-    String typeMonster;
+    private String typeMonster;
     Scanner input = new Scanner(System.in);
-    String nameMonster;
-    int ageMonster;
-    boolean badBreathMonster;
-    double gpaMonster;
-    double numOfYears;
-    double price;
-    double newAge;
-    double gpaMultiplier;
+    private String nameMonster;
+    private int ageMonster;
+    private boolean badBreathMonster;
+    private double gpaMonster;
+    // private double gpaMonsterTest;
+    // private String nameMonsterTest;
+    // private String typeMonsterTest;
+    // private int ageMonsterTest;
+    // private boolean bbMonsterTest;
 
-    public Monster(String type, String name, int age, boolean badBreath, double gpa) {
+    /*
+    public Monster(String typeTest, String nameTest, int ageTest, boolean badBreathTest, double gpaTest) {
         // System.out.println("What is the type of your new monster?: ");
         // String type = input.nextLine();
         // type = typeMonster;
-        this.typeMonster = type;
+        this.typeMonsterTest = typeTest;
         // System.out.println("What is the name of your new monster?: ");
         // String name = input.nextLine();
         // name = nameMonster;
-        this.nameMonster = name;
+        this.nameMonsterTest = nameTest;
         // System.out.println("What is the age of your new monster?: ");
         // int age = input.nextInt();
         // age = ageMonster;
-        this.ageMonster = age;
+        this.ageMonsterTest = ageTest;
         // System.out.println("Does your monster have bad breath? (True/False): ");
         // boolean badBreath = input.nextBoolean();
         // badBreath = badBreathMonster;
-        this.badBreathMonster = badBreath;
+        this.bbMonsterTest = badBreathTest;
         // System.out.println("What is the gross point average of your new monster?: ");
         // double gpa = input.nextDouble();
         // gpa = gpaMonster;
-        this.gpaMonster = gpa;
+        this.gpaMonsterTest = gpaTest;
+        /*
         System.out.println();
+        System.out.println("MONSTER 2");
+        System.out.println("---------------");
         System.out.println("type: " + typeMonster);
         System.out.println("name: " + nameMonster);
         System.out.println("age: " + ageMonster);
         System.out.println("bad breath: " + badBreathMonster);
         System.out.println("gpa: " + gpaMonster);
+        System.out.println("---------------");
         System.out.println();
+
+         */
+    // }
+
+    public String changeType(String newType) {
+        typeMonster = newType;
+        // System.out.println("New type: " + typeMonster);
+        return typeMonster;
+    }
+
+    public String changeName(String newName) {
+        nameMonster = newName;
+        // System.out.println("New name: " + nameMonster);
+        return nameMonster;
+    }
+
+    public int changeAge(int newAge) {
+        ageMonster = newAge;
+        // System.out.println("New age: " + ageMonster);
+        return ageMonster;
+    }
+
+    public boolean changeBadBreath(boolean newBadBreath) {
+        badBreathMonster = newBadBreath;
+        // System.out.println("New bad breath: " + badBreathMonster);
+        return badBreathMonster;
+    }
+
+    public double changeGPA(double newGPA) {
+        gpaMonster = newGPA;
+        // System.out.println("New GPA: " + gpaMonster);
+        return gpaMonster;
+    }
+
+    public Monster(String type, String name, int age, boolean badBreath, double gpa) {
+        // System.out.println("What is the type of your new monster?: ");
+        // String type = input.nextLine();
+        // type = typeMonster;
+        typeMonster = type;
+        // System.out.println("What is the name of your new monster?: ");
+        // String name = input.nextLine();
+        // name = nameMonster;
+        nameMonster = name;
+        // System.out.println("What is the age of your new monster?: ");
+        // int age = input.nextInt();
+        // age = ageMonster;
+        ageMonster = age;
+        // System.out.println("Does your monster have bad breath? (True/False): ");
+        // boolean badBreath = input.nextBoolean();
+        // badBreath = badBreathMonster;
+        badBreathMonster = badBreath;
+        // System.out.println("What is the gross point average of your new monster?: ");
+        // double gpa = input.nextDouble();
+        // gpa = gpaMonster;
+        gpaMonster = gpa;
+        /*
+        System.out.println();
+        System.out.println("MONSTER 2");
+        System.out.println("---------------");
+        System.out.println("type: " + typeMonster);
+        System.out.println("name: " + nameMonster);
+        System.out.println("age: " + ageMonster);
+        System.out.println("bad breath: " + badBreathMonster);
+        System.out.println("gpa: " + gpaMonster);
+        System.out.println("---------------");
+        System.out.println();
+
+         */
     }
 
 
 
     public Monster() {
         typeMonster = "Zombie";
-        String nameMonster = "Uuuggghhh";
+        nameMonster = "Uuuggghhh";
         ageMonster = 0;
         badBreathMonster = true;
         gpaMonster = 3.9;
+        /*
         System.out.println();
-        System.out.println("DEFAULT MONSTER");
+        System.out.println("MONSTER 1");
         System.out.println("---------------");
         System.out.println(typeMonster);
         System.out.println(nameMonster);
         System.out.println(ageMonster);
         System.out.println(badBreathMonster);
         System.out.println(gpaMonster);
+        System.out.println("---------------");
+
+         */
 
     }
 
-    public Monster(String nameMonster3, int ageMonster3) {
-        this.nameMonster = nameMonster3;
-        this.ageMonster = ageMonster3;
+    public Monster(String nameMonster, int ageMonster) {
+        this.nameMonster = nameMonster;
+        this.ageMonster = ageMonster;
         double min = 0.0;
         double max = 4.0;
         double randomDouble = (double)Math.floor(Math.random()*(max-min+1)+min);
-        typeMonster = "Vampire";
-        badBreathMonster = false;
-        gpaMonster = randomDouble;
+        this.typeMonster = "Vampire";
+        this.badBreathMonster = false;
+        this.gpaMonster = randomDouble;
+        // System.out.println(gpaMonster);
+        /*
         System.out.println();
-        System.out.println("Type m3: " + typeMonster);
-        System.out.println("Name m3: " + nameMonster3);
-        System.out.println("Age m3: " + ageMonster3);
-        System.out.println("Bad breath m3: " + badBreathMonster);
-        System.out.println("GPA m3: " + gpaMonster);
+        System.out.println("MONSTER 3");
+        System.out.println("---------------");
+        System.out.println("Type: " + typeMonster);
+        System.out.println("Name: " + nameMonster3);
+        System.out.println("Age: " + ageMonster3);
+        System.out.println("Bad breath: " + badBreathMonster);
+        System.out.println("GPA: " + gpaMonster);
+        System.out.println("---------------");
+
+         */
     }
 
     void timeWarp(double numberOfYears) {
-        this.numOfYears = numberOfYears;
-        this.newAge = ageMonster + numberOfYears;
-        this.gpaMultiplier = 0.1 * ((int) (Math.floor((double) numberOfYears / 100.0)));
-        System.out.println(gpaMultiplier);
-        double newGPA = gpaMonster * gpaMultiplier;
-        BigDecimal roundedGPA = new BigDecimal(newGPA).setScale(2, RoundingMode.HALF_UP);
-        System.out.println("The Monster's new GPA is " + roundedGPA);
+        this.ageMonster += (int) numberOfYears;
+        double gpaMultiplier = 0.1 * ((int) (Math.floor((double) numberOfYears / 100.0)));
+        // System.out.println(gpaMultiplier);
+        double newGPA = this.gpaMonster * gpaMultiplier;
+        BigDecimal roundedGPA = new BigDecimal(newGPA).setScale(1, RoundingMode.HALF_UP);
+        // Extra feature (***)
+        if (newGPA > 4.0) {
+            roundedGPA = BigDecimal.valueOf(4.0);
+        }
+        this.gpaMonster = roundedGPA.doubleValue();
+        // System.out.println(gpaMonster);
+        // System.out.println("The Monster's new age is " + this.ageMonster);
+        // System.out.println("The Monster's new GPA is " + roundedGPA);
     }
 
-    void spaDay(int age, double gpa, double priceSpaDay) {
-        this.price = priceSpaDay;
-        double newAge = (int) (age - price);
+    void spaDay(int ageMonster, double gpaMonster, double priceSpaDay) {
+        this.ageMonster = (int) (ageMonster - priceSpaDay);
         boolean badBreathMonster = false;
-        gpaMonster = gpa / 2;
-        if (newAge < 0) {
-            newAge = 0;
+        this.gpaMonster = gpaMonster / 2;
+        // Extra feature (***)
+        if (this.ageMonster < 0) {
+            this.ageMonster = 0;
         }
+        /*
         System.out.println("New bad breath: " + badBreathMonster);
-        System.out.println("New gpa: " + gpaMonster);
+        System.out.println("New gpa: " + this.gpaMonster);
         System.out.println("Price: " + priceSpaDay);
-        System.out.println("New age: " + newAge);
+        System.out.println("New age: " + this.ageMonster);
+
+         */
+
     }
+
+    BigDecimal percentEvil() {
+        double indexOf = nameMonster.indexOf("a");
+        double lengthName = nameMonster.length();
+        BigDecimal percent2Decimal = null;
+        try {
+            double percentEvil = ((indexOf + 1) / lengthName) * 100;
+            percent2Decimal = new BigDecimal(percentEvil).setScale(1, RoundingMode.HALF_UP);
+            System.out.print("Evil percentage of " + nameMonster + " is: " + percent2Decimal + "\n");
+            // System.out.print("Percent Evil: " + percent2Decimal);
+
+        } catch (Exception e) {
+            System.out.println("Cannot divide by zero.");
+        }
+        return percent2Decimal;
+        // BigDecimal percent2Decimal = new BigDecimal(percentEvil).setScale(2, RoundingMode.HALF_UP);
+        // System.out.println("Evil percentage of " + nameMonster3 + " is: " + percent2Decimal);
+    }
+    public String toString() {
+        return "Type: " + this.typeMonster + " | " +
+                "Name: " + this.nameMonster + " | " +
+                "Age: " + this.ageMonster + " | " +
+                "Bad Breath?: " + this.badBreathMonster + " | " +
+                "GPA: " + this.gpaMonster + " | " +
+                "Percent Evil: " + percentEvil() + " | \n";
+
+    }
+    // public String nameArray() {
+
+        // }
+
+        // return Arrays.toString(arr);
+    // }
 }
